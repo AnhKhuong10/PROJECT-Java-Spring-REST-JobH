e38e3fd48a633fc2432bf462f9193de2458e470f
Anh Khuong <anhkhuong989@gmail.com>
2025-02-18 01:34:53 +0700

    format json

--- src/main/java/vn/hoidanit/jobhunter/domain/dto/ResLoginDTO.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/dto/ResLoginDTO.java
@@ -10,6 +10,8 @@ import lombok.Setter;
 @Setter
 @Getter
 public class ResLoginDTO {
+
+    @JsonProperty("access_token")
     private String accessToken;
     private UserLogin user;
 
@@ -21,6 +23,14 @@ public class ResLoginDTO {
         private long id;
         private String email;
         private String name;
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class UserGetAccount {
+        private UserLogin user;
     }
 
 }
